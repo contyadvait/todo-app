1c9cc2a785b04f5d7443665e9cbf9da8e38fa286
contyadvait <advaitthepro@outlook.com>
2023-07-29 16:49:31 +0800

    Initial Commit

---
+++ To Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To Do
+//
+//  Created by Milind Contractor on 29/7/23.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
